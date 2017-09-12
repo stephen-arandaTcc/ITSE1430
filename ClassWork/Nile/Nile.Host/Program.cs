0a9d26b9eead0f63a94c9179d7a942cdf297b502
stephen.aranda <stephen.aranda@my.tccd.edu>
2017-09-11 19:07:50 -0500

    9/11/2017 class work product

--- ClassWork/Nile/Nile.Host/Program.cs
+++ ClassWork/Nile/Nile.Host/Program.cs
@@ -8,7 +8,91 @@ namespace Nile.Host
 {
     class Program 
     {
-        static void Main( string[] args )
+        static void Main(string [] args)
+        {
+            bool quit = false;           
+            do
+            {
+                char choice = GetInput();
+                switch (choice)
+                {
+                    case 'a':
+                    case 'A': AddProduct(); break;
+
+                    case 'l':
+                    case 'L': ListProduct(); break;
+
+                    case 'q':
+                    case 'Q': quit = true; break;
+                };
+            } while (!quit);
+
+        }
+
+        private static void AddProduct()
+        {
+            Console.Write("Enter product name: ");
+            productName = Console.ReadLine().Trim();
+
+            // Ensure not empty
+
+            Console.Write("Enter price ( > 0): ");
+            string price = Console.ReadLine();
+
+            Console.Write("Enter optional description: ");
+            productDescription = Console.ReadLine().Trim();
+
+            Console.Write("Is it discontinued (Y/N): ");
+            string discontinued = Console.ReadLine().Trim();
+        }
+
+        private static void ListProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        static char GetInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("".PadLeft(10,'-'));
+                Console.WriteLine("A)add Product");
+                Console.WriteLine("L)ist Products");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine().Trim();
+
+                // option 1 = string literal
+                // if(input !="")
+
+                // option 2 - string field
+                // if (input != String.Empty)
+
+                // option 3 - length              
+                if (input != null && input.Length != 0)
+                {
+                    // string comparison
+                    if (String.Compare(input, "A", true) == 0)
+                        return 'A';
+
+                    // char comparison
+                    char letter = Char.ToUpper(input[0]);
+                    if (letter == 'A')
+                        return 'A';
+                    else if (letter == 'L')
+                        return 'L';
+                    else if (letter == 'Q')
+                        return 'Q';
+                }           
+
+                // Error
+                Console.WriteLine("Please choose a valid option");
+            };
+
+        }
+
+        static void Main2( string[] args )
         {
             int hours = 5;
             hours = 10;
@@ -39,12 +123,24 @@ namespace Nile.Host
             // option 3
             string names3 = String.Concat("John", " william", " Murphy", " Charles", " Henry");
 
-            // string formatting
-            // John worked 10 hours
+            // string formatting - John worked 10 hours
             string format1 = name + " worked" + hours.ToString() + " hours";
 
             string format2 = String.Format("{0} worked for {1} hours", name, hours);
-           
+            
+            // end of last week examples for formatting strings
+
+            // new section
+            // option 3 - string interpulation mode - any expression allowed
+            string format3 = $"{name} worked for {hours} hours";
+
         }
+
+        // Product
+        static string productName;
+        static decimal productPrice;
+        static string productDescription;
+        static bool productDiscontinued;
+        
     }
 }
