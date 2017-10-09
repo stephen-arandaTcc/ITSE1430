44909cbed2661e7f176fb379995bda3a67ca49d9
stephen <stephen.aranda@my.tcc.edu>
2017-10-09 10:10:34 -0500

    10/9/2017 Lab2. Stephen Aranda. Ready to be graded.

---
+++ Labs - Copy/Lab2/MovieLib.Windows/MainForm.cs
@@ -0,0 +1,93 @@
+﻿// Stephen Aranda
+// 10/9/2017
+// ITSE 1430
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieLib.Windows
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            Close();
+        }
+
+        private void OnMovieAdd( object sender, EventArgs e )
+        {
+            var child = new MovieDetailForm("Movie Details");
+
+            if (child.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            _movie = child.Movie;
+        }
+
+       
+
+        private void OnMovieEdit( object sender, EventArgs e )
+        {
+            var child = new MovieDetailForm("Movie Details");
+            child.Movie = _movie;
+            if (_movie == null)
+            {
+                MessageBox.Show(this, "Movie has not yet been defined!");
+                return;
+            };
+            if (child.ShowDialog(this) != DialogResult.OK)               
+                return;
+            
+
+            _movie = child.Movie;
+        }
+
+        
+
+        private void OnMovieDelete( object sender, EventArgs e )
+        {
+            if (_movie == null)
+            {
+                MessageBox.Show(this, "Movie has not yet been defined!");
+                return;
+            };
+                
+
+            if (MessageBox.Show(this, $"Are you sure you want to delete '{_movie.Title}'? ",
+                               "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                return;
+            
+               
+
+            _movie = null;
+        }
+
+        private Movie _movie;
+
+        private void OnHelpAbout( object sender, EventArgs e )
+        {
+            var about = new AboutMovieLib();
+            about.ShowDialog(this);
+        }
+
+        public delegate void ButtonClickCall( object sender, EventArgs e );
+
+        private void CallButton( ButtonClickCall functionToCall )
+        {
+            functionToCall(this, EventArgs.Empty);
+        }
+
+       
+    }
+}
---
+++ Labs - Copy/Lab2/MovieLib/Movie.cs
@@ -0,0 +1,75 @@
+﻿// Stephen Aranda
+// 10/9/2017
+// ITSE 1430
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>Represents a movie.</summary>
+/// <remarks>
+/// This will represent the information of the movie.
+/// </remarks>
+namespace MovieLib
+{
+    public class Movie
+    {
+        public Movie()
+        {
+
+        }
+
+
+        /// <value>Never returns a null</value>
+        public string Title
+        {
+            
+            get {
+                return _title ?? "";
+            }
+
+            
+            set {
+                _title = value?.Trim();
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description ?? "";
+            }
+
+            set
+            {
+                _description = value?.Trim();
+            }
+        }
+
+        public int Length { get; set; } = 0;
+
+        public bool IsOwned { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+
+        public virtual string Validate()
+        {
+            if (String.IsNullOrEmpty(Title))
+                return "Name cannot be empty.";
+
+            if (Length < 0)
+                return "Length must be >= 0.";
+
+            return null;
+        }
+
+        private string _title;
+        private string _description;
+    }
+}
