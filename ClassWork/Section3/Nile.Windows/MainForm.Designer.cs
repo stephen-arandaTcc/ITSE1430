be354034bdeb491a067ec04aa843e9087df69b3a
stephen.aranda <stephen.aranda@my.tccd.edu>
2017-10-30 19:13:14 -0500

    classwork 10/30/2017

---
+++ ClassWork/Section3/Nile.Windows/MainForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Nile.Windows
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this._mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._miFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._miProductAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this._miProductEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this._miProductDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._miHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this._gridProducts = new System.Windows.Forms.DataGridView();
+            this._bsProducts = new System.Windows.Forms.BindingSource(this.components);
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIsDiscontinued = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._mainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._gridProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._bsProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _mainMenu
+            // 
+            this._mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.productToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this._mainMenu.Location = new System.Drawing.Point(0, 0);
+            this._mainMenu.Name = "_mainMenu";
+            this._mainMenu.Size = new System.Drawing.Size(663, 24);
+            this._mainMenu.TabIndex = 0;
+            this._mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // _miFileExit
+            // 
+            this._miFileExit.Name = "_miFileExit";
+            this._miFileExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this._miFileExit.Size = new System.Drawing.Size(134, 22);
+            this._miFileExit.Text = "E&xit";
+            this._miFileExit.Click += new System.EventHandler(this.OnFileExit);
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miProductAdd,
+            this._miProductEdit,
+            this.toolStripSeparator1,
+            this._miProductDelete});
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            this.productToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.productToolStripMenuItem.Text = "&Product";
+            // 
+            // _miProductAdd
+            // 
+            this._miProductAdd.Name = "_miProductAdd";
+            this._miProductAdd.ShortcutKeys = System.Windows.Forms.Keys.Insert;
+            this._miProductAdd.Size = new System.Drawing.Size(131, 22);
+            this._miProductAdd.Text = "&Add";
+            this._miProductAdd.Click += new System.EventHandler(this.OnProductAdd);
+            // 
+            // _miProductEdit
+            // 
+            this._miProductEdit.Name = "_miProductEdit";
+            this._miProductEdit.Size = new System.Drawing.Size(131, 22);
+            this._miProductEdit.Text = "&Edit";
+            this._miProductEdit.Click += new System.EventHandler(this.OnProductEdit);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(128, 6);
+            // 
+            // _miProductDelete
+            // 
+            this._miProductDelete.Name = "_miProductDelete";
+            this._miProductDelete.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this._miProductDelete.Size = new System.Drawing.Size(131, 22);
+            this._miProductDelete.Text = "&Delete";
+            this._miProductDelete.Click += new System.EventHandler(this.OnProductDelete);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._miHelpAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // _miHelpAbout
+            // 
+            this._miHelpAbout.Name = "_miHelpAbout";
+            this._miHelpAbout.Size = new System.Drawing.Size(107, 22);
+            this._miHelpAbout.Text = "&About";
+            this._miHelpAbout.Click += new System.EventHandler(this.OnHelpAbout);
+            // 
+            // _gridProducts
+            // 
+            this._gridProducts.AllowUserToAddRows = false;
+            this._gridProducts.AllowUserToDeleteRows = false;
+            this._gridProducts.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this._gridProducts.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this._gridProducts.AutoGenerateColumns = false;
+            this._gridProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._gridProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colName,
+            this.colDescription,
+            this.colPrice,
+            this.colIsDiscontinued,
+            this.colId});
+            this._gridProducts.DataSource = this._bsProducts;
+            this._gridProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._gridProducts.Location = new System.Drawing.Point(0, 24);
+            this._gridProducts.Name = "_gridProducts";
+            this._gridProducts.ReadOnly = true;
+            this._gridProducts.RowHeadersVisible = false;
+            this._gridProducts.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this._gridProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this._gridProducts.Size = new System.Drawing.Size(663, 284);
+            this._gridProducts.TabIndex = 1;
+            this._gridProducts.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnEditRow);
+            this._gridProducts.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDownGrid);
+            // 
+            // colName
+            // 
+            this.colName.DataPropertyName = "Name";
+            this.colName.FillWeight = 200F;
+            this.colName.Frozen = true;
+            this.colName.HeaderText = "Name";
+            this.colName.MaxInputLength = 100;
+            this.colName.MinimumWidth = 200;
+            this.colName.Name = "colName";
+            this.colName.ReadOnly = true;
+            this.colName.Width = 200;
+            // 
+            // colDescription
+            // 
+            this.colDescription.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colDescription.DataPropertyName = "Description";
+            this.colDescription.FillWeight = 300F;
+            this.colDescription.HeaderText = "Description";
+            this.colDescription.MinimumWidth = 200;
+            this.colDescription.Name = "colDescription";
+            this.colDescription.ReadOnly = true;
+            // 
+            // colPrice
+            // 
+            this.colPrice.DataPropertyName = "Price";
+            dataGridViewCellStyle2.Format = "C2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.colPrice.DefaultCellStyle = dataGridViewCellStyle2;
+            this.colPrice.HeaderText = "Price";
+            this.colPrice.MinimumWidth = 100;
+            this.colPrice.Name = "colPrice";
+            this.colPrice.ReadOnly = true;
+            // 
+            // colIsDiscontinued
+            // 
+            this.colIsDiscontinued.DataPropertyName = "IsDiscontinued";
+            this.colIsDiscontinued.HeaderText = "Is Discontineud?";
+            this.colIsDiscontinued.Name = "colIsDiscontinued";
+            this.colIsDiscontinued.ReadOnly = true;
+            this.colIsDiscontinued.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // colId
+            // 
+            this.colId.DataPropertyName = "Id";
+            this.colId.HeaderText = "ID";
+            this.colId.Name = "colId";
+            this.colId.ReadOnly = true;
+            this.colId.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 308);
+            this.Controls.Add(this._gridProducts);
+            this.Controls.Add(this._mainMenu);
+            this.MainMenuStrip = this._mainMenu;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Nile";
+            this._mainMenu.ResumeLayout(false);
+            this._mainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._gridProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._bsProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip _mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _miFileExit;
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _miProductAdd;
+        private System.Windows.Forms.ToolStripMenuItem _miProductEdit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem _miProductDelete;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _miHelpAbout;
+        private System.Windows.Forms.DataGridView _gridProducts;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colIsDiscontinued;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.BindingSource _bsProducts;
+    }
+}
+
---
+++ ClassWork/Section3/Nile/Stores/ProductDatabase.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Stores
+{
+    /// <summary>Base class for product database.</summary>
+    public abstract class ProductDatabase : IProductDatabase
+    {        
+        /// <summary>Adds a product.</summary>
+        /// <param name="product">The product to add.</param>
+        /// <returns>The added product.</returns>
+        /// <exception cref="ArgumentNullException">Product is null.</exception>
+        /// <exception cref="ValidationException">Product is invalid.</exception>
+        public Product Add ( Product product )
+        {
+            // Validate
+            if (product == null)
+                throw new ArgumentNullException(nameof(product),"Product was null.");
+            //return null;
+
+            //Using IValidatableObject
+            //if (!ObjectValidator.TryValidate(product, out var errors))
+            //    throw new System.ComponentModel.DataAnnotations.ValidationException("Product was not valid.", nameof(product));
+            //    //return null;
+            ObjectValidator.Validate(product);
+
+
+            try
+            {
+                return AddCore(product);
+
+            } catch (Exception e)
+            {
+                // Throw different exception
+                throw new Exception("Add failed", e);
+
+                //Re-throw- 
+                throw;
+
+                // Silently ignore - almost always bad
+                
+            };
+
+           
+            
+            
+        }
+
+        /// <summary>Get a specific product.</summary>
+        /// <returns>The product, if it exists.</returns>
+        public Product Get ( int id )
+        {
+            //Validate
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0.");
+               // return null;
+
+            return GetCore(id);
+        }
+        
+        /// <summary>Gets all products.</summary>
+        /// <returns>The products.</returns>
+        public IEnumerable<Product> GetAll ()
+        {
+            return GetAllCore();
+
+            #region Ignore
+
+            //How many products?
+            //var count = 0;
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        ++count;
+            //};
+
+            //var items = new Product[count];
+            //var index = 0;
+
+            //foreach (var product in _products)
+            //{
+            //    if (product != null)
+            //        //product = new Product();
+            //        items[index++] = CopyProduct(product);
+            //};
+
+            //return items;
+            #endregion
+        }
+        
+        /// <summary>Removes the product.</summary>
+        /// <param name="id">The product to remove.</param>
+        public void Remove ( int id )
+        {
+            //TODO: Validate
+            if (id <= 0)
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0.");
+
+            RemoveCore(id);
+
+            //if (_list[index].Name == product.Name)
+            //{
+            //    _list.RemoveAt(index);
+            //    break;
+            //};        
+        }
+        
+        /// <summary>Updates a product.</summary>
+        /// <param name="product">The product to update.</param>
+        /// <returns>The updated product.</returns>
+        public Product Update ( Product product )
+        {
+            //TODO: Validate
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            //Using IValidatableObject
+            if (!ObjectValidator.TryValidate(product, out var errors))
+                throw new ArgumentException("Product is invalid.", nameof(product));
+
+            //if (!String.IsNullOrEmpty(product.Validate()))
+            //    return null;
+            ObjectValidator.Validate(product);
+
+            // Use throw expression
+            //Get existing product
+            var existing = GetCore(product.Id) ?? throw new Exception("Product not found.");
+            //if (existing == null)
+            //    throw new Exception("Product not found.");
+
+            return UpdateCore(existing, product);
+        }
+
+        #region Protected Members
+
+        protected abstract Product GetCore( int id );
+
+        protected abstract IEnumerable<Product> GetAllCore();
+
+        protected abstract void RemoveCore( int id );
+
+        protected abstract Product UpdateCore( Product existing, Product newItem );
+
+        protected abstract Product AddCore( Product product );
+        #endregion
+    }
+}
