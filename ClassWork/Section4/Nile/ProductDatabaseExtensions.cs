127ffd0c1ed9090d35132a9a866399ded78ad659
stephen.aranda <stephen.aranda@my.tccd.edu>
2017-11-08 18:42:05 -0600

    11/8/2017 classwork

--- ClassWork/Section4/Nile/ProductDatabaseExtensions.cs
+++ ClassWork/Section4/Nile/ProductDatabaseExtensions.cs
@@ -2,6 +2,8 @@
  * ITSE 1430
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nile.Stores
 {
@@ -19,6 +21,42 @@ namespace Nile.Stores
             return null;
 
         }
+
+
+        public static IEnumerable<Product> GetProductsByDiscountPrice ( this IProductDatabase source,
+                                                                        Func<Product, decimal> priceCalculator)
+        {
+            var products = from product in source.GetAll()
+                           where product.IsDiscontinued
+                           //select new SomeType() {
+                           select new  {
+                               Product = product,
+                               AdjustedPrice = product.IsDiscontinued ? priceCalculator(product) : product.Price
+                           };
+
+            //Instead of anonymous type
+            // var tuple = Tuple.Create<Product, decimal>(new Product(), 10M);
+            
+
+
+            return from product in products
+                   orderby product.AdjustedPrice
+                   select product.Product;
+
+        }
+
+        // Example of a tuple
+        //private (Product, decimal) DoSomething()
+        //{
+        //    return (newProduct, 10M);
+        //}
+        //private sealed class SomeType 
+        //{
+        //    public Product Product { get; set; }
+        //    public decimal AdjustedPrice { get; set; }
+
+        //}
+
         /// <summary>Adds seed data to a database.</summary>
         /// <param name="database">The data to seed.</param>
         public static void WithSeedData ( this IProductDatabase source )
